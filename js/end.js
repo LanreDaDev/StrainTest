f27fc6e1d0811bf78f5cc55e372aac401e110e38
Stingre9445 <Stingre9445@gmail.com>
2019-10-11 19:50:06 -0600

    Brought ajax call in, Still figuring out how to write the end page code

--- js/end.js
+++ js/end.js
@@ -1,41 +1,74 @@
-// const username = document.getElementById('username');
-// const saveScoreBtn = document.getElementById('saveScoreBtn');
-// const finalScore = document.getElementById('finalScore')
-// const mostRecentScore = localStorage.getItem('mostRecentScore');
+// const username = document.getElementById("username");
+// const saveScoreBtn = document.getElementById("saveScoreBtn");
+// const finalScore = document.getElementById("finalScore");
+// const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 // finalScore.innerText = mostRecentScore;
 
-
 // username.addEventListener("keyup", () => {
-//     saveScoreBtn.disabled = !username.value;
-
-// })
+//   saveScoreBtn.disabled = !username.value;
+// });
 
 // saveHighScore = e => {
-//     console.log('clicked the save button!');
-//     e.preventDefault();
+//   console.log("clicked the save button!");
+//   e.preventDefault();
+// };
+const perfectStrains = JSON.parse(localStorage.getItem("perfectStrains"));
+const key = "xaizI2o";
+const url = "strainapi.evanbusse.com/";
 
-// }
-const perfectStrains = JSON.parse(localStorage.getItem("perfectStrains"))
+resultStrains = [...perfectStrains];
+resultStrains.forEach(function(e) {
+  if (e.name == "") {
+  }
+  // console.log(e.name);
+});
+console.log(resultStrains[0].name);
 
+let id = 5;
+let name = "Granddaddy Purple";
 
-startResults = () =>{
-    strainCounter = 0;
-    resultStrains = [...perfectStrains];
-    const maxStrains = resultStrains.length;
+let URL = search_ByName(url, key, name);
 
-    getResults();
-}
+ajaxRequest(URL);
 
+function ajaxRequest(URL) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://" + URL, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let res = xhr.response;
+      let data = JSON.parse(res);
+      // let identification = data[0].id;
+      // let URLEffects = search_ByStrainEffect(url, key, identification);
+      // ajaxRequest(URLEffects);
+      console.log(data);
 
-getResults = () =>{
-
-    if (resultStrains === 0 || strainCounter >= maxStrains) {
-        
+      //   display_Data(data);
     }
+  };
 
-
+  xhr.send();
 }
+
+function search_ByName(url, key, name) {
+  return url + key + "/strains/search/name/" + name.toLowerCase();
+}
+
+// startResults = () => {
+//   strainCounter = 0;
+//   resultStrains = [...perfectStrains];
+//   const maxStrains = resultStrains.length;
+
+//   resultStrains.foreach(function(e) {
+//     console.log(e);
+//   });
+//   getResults();
+// };
+
+// getResults = () => {
+//   if (resultStrains === 0 || strainCounter >= maxStrains) {
+//   }
+// };
 
 // console.log(perfectStrains);
-
